19ba90741eb5b15126d5bd637becd38ac1aba249
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2026-01-25 13:13:02 +0530

    frontend init

---
+++ frontend/src/SchoolStudent.tsx
@@ -0,0 +1,18 @@
+const SchoolStudent = () => {
+    return (
+        <>
+            <h1>School Student</h1>
+            <input type="text" placeholder="Enter your name" />
+            <input type="text" placeholder="Enter your school name" />
+            <input type="text" placeholder="Enter your password" />
+            <input type="number" placeholder="Enter your parent phone number" />
+            <input type="text" placeholder="Enter your standard" />
+            <input type="number" placeholder="Enter your address" />
+            <input type="text" placeholder="Enter referral code" />
+            <input type="text" placeholder="Enter your feedback details" />
+            <button onClick={() => console.log("Submitted")}>Submit</button>
+        </>
+    )
+}
+
+export default SchoolStudent
